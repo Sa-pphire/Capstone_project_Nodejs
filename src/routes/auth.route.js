e930bccbef09237db7512a1727f460834dd37706
Oladimeji Ibitoye <stephen_ibitoye@yahoo.com>
2022-05-23 17:24:46 +0100

    updated the auth route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -17,16 +17,4 @@ router.route('/signup')
 router.route('/signin')
     .post(signinValidator, asyncHandler(authController.signin));
 
-router.route('/property')
-    .post(checkAuth, propertyValidator,  asyncHandler(authController.property))
-
-router.route('/property/:property_id')
-    .patch(checkAuth, propertyValidator, asyncHandler(authorizedProperty), asyncHandler(authController.updateProperty) )
-
-router.route('/property/:property_id/sold')
-    .patch(checkAuth, asyncHandler(authorizedProperty), asyncHandler(authController.soldProperty))
-
-router.route('/property/:property_id')
-    .delete(checkAuth, asyncHandler(authorizedProperty), asyncHandler(authController.deleteProperty))
-
 module.exports = router;
